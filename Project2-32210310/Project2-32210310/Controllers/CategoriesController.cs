64e8ce9aaf77f8fc6c8f881425b141d5625b39e7
NE NZANZA <32210310@student365.msfed.nwu.ac.za>
2022-09-07 20:32:52 +0200

    migrated tables

--- Project2-32210310/Project2-32210310/Controllers/CategoriesController.cs
+++ Project2-32210310/Project2-32210310/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,8 @@ using Project2_32210310.Models;
 
 namespace Project2_32210310.Controllers
 {
+    [Authorize]
+
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
